50d7373ea39b58c9d498a87864e46fa106e91650
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-11-07 18:23:02 +0200

    Add project files.

---
+++ Existek_Lesson_1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Existek_Lesson_1._2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 67;
+
+            
+        }
+    }
+
+    namespace Book
+    {
+        
+    }
+}
