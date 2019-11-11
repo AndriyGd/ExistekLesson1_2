ec5de14a4a09d7f8ee6598da21087fdfc2094127
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-11-11 18:18:51 +0200

    Added Project SimpleClasses

---
+++ Existek_Lesson_1_2.SimpleClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Existek_Lesson_1_2.SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
