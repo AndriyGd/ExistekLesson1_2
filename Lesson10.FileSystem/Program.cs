a401d851bf812bcdc3deb2d43e357a165c0573cb
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-11-27 19:13:02 +0200

    Example of Stream Writer

--- Lesson10.FileSystem/Program.cs
+++ Lesson10.FileSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lesson10.FileSystem
 {
@@ -10,6 +11,26 @@ namespace Lesson10.FileSystem
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter text to write to file: ");
+            var text = Console.ReadLine();
+
+            WriteToFile("Lesson10.txt", text);
+
+            Console.ReadLine();
+        }
+
+        private static void WriteToFile(string fileName, string text)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                var sw = new StreamWriter(fs);
+                sw.WriteLine(text);
+                sw.Flush();
+                Console.WriteLine("Test was wrote to file.");
+
+                //var res = true;
+            }
+
         }
     }
 }
