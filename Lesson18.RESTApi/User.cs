1133d71566ea63fafd3e6bcf996c8ab4782fa7f6
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-12-12 20:29:59 +0200

    Added class User

---
+++ Lesson18.RESTApi/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson18.RESTApi
+{
+    /*
+     * "id": 1,
+       "name": "Leanne Graham",
+       "username": "Bret",
+       "email": "Sincere@april.biz",
+       "phone": "1-770-736-8031 x56442",
+       "website": "hildegard.org",
+     */
+    class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+    }
+}
