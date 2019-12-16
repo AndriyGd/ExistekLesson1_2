a1166b80c348a788ad87d776e9d95a56e9e296be
Andrew Gladkyi <andriy.gladkyi@existek.com>
2019-12-16 20:34:18 +0200

    Added Project Lesson18.EntityFramework

--- Lesson18.EntityFramework/Program.cs
+++ Lesson18.EntityFramework/Program.cs
@@ -10,6 +10,50 @@ namespace Lesson18.EntityFramework
     {
         static void Main(string[] args)
         {
+            var db = new UserDataContext();
+#if false
+            //var users = db.Users.ToList();
+
+            //Console.WriteLine($"OK: {users.Count}");
+
+            var user = new User
+            {
+                UserName = "Olha",
+                Password = "HN-90-ua"
+            };
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            Console.WriteLine($"User Id: {user.UserId}"); 
+#endif
+            //PrintUsers(db);
+            //Console.WriteLine();
+            //PrintUsers(db);
+
+            var user = db.Users.FirstOrDefault();
+            if (user != null)
+            {
+                //Console.WriteLine(user.Password);
+                //user.Password = "JOK-POL-89";
+                //db.SaveChanges();
+
+                Console.WriteLine(user.UserName);
+ 
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void PrintUsers(UserDataContext db)
+        {
+            var users = from u in db.Users where u.UserId >= 2 orderby u.UserName select u;
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.UserName}; Password: {user.Password}");
+            }
         }
     }
 }
---
+++ Lesson18.EntityFramework/UserDataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Lesson18.EntityFramework
+{
+    public class UserDataContext: DbContext
+    {
+
+        public UserDataContext()
+        {
+            
+        }
+
+        public UserDataContext(string connectionString) : base(connectionString)
+        {
+            
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
